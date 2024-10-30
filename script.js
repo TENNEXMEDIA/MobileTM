63907e4343e10622df80b3b26dc309be999b5e34
TENNEXMEDIA <TENNEXMEDIAMARKETING@GMAIL.COM>
2024-10-30 22:15:03 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Hero Section interaction
+function revealButtons() {
+    document.getElementById("extra-buttons").style.display = "block";
+}
+
+// Add other JavaScript functions here as you build
